22e9992d47528a11bc8e2be90336c2b9c5b3567f
Aditya Rudola <adityarrudola@gmail.com>
2026-02-26 16:57:01 +0530

    Update header text in App component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>React App Running on Azure ACI</h1>
+      <h1>React App Running on Azure ACI - changes made</h1>
     </div>
   );
 }
